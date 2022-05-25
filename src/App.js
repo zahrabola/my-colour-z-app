0889eeb979b91c2141db41c0d31a7c643a0214d6
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-25 11:46:58 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+
+const[colour, setColour] = useState("");
+const [error, setError] = useState(false);
+const [list,setList]= useState([]);
+
+
+const handleSubmit =(e)=> {
+  e.preventDefault ();
+  console.log('hello');
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
+
+        
         <h1>Colour Generator </h1>
       </header>
     </div>
