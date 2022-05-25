cd307d3df8231ef000812ff865772bc3029d6d0c
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-25 12:55:51 +0100

    Update Singlecolour

--- src/Singlecolour.js
+++ src/Singlecolour.js
@@ -1,1 +1,17 @@
+import React, { useState, useEffect } from "react";
+const Singlecolour = ({rgb, weight, index }) => {
+      return (
+
+<div>
+    <h4>Single Colour</h4>
+</div>
+
+
+      )
+}
+
+
+
+
+
 export default Singlecolour;
